b381751f40931f0b8fdcc1bf14142d100aabeec6
poisenmj <maksjl01@gmail.com>
2020-05-21 13:35:25 +0100

    First commit to spotify branch

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+from keys import spotify_client_id, spotify_client_secret
+os.environ['SPOTIPY_CLIENT_ID'] = spotify_client_id
+os.environ['SPOTIPY_CLIENT_SECRET'] = spotify_client_secret
+
+spotify_api = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 app = Flask(__name__)
 Bootstrap(app)
